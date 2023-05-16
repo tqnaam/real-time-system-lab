e776e48d2cbc808b9ed04942a92b529584053133
tqnaam <18770@student.vgu.edu.vn>
2023-05-16 13:41:40 +0700

    update main task 1

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
-
-scheduller.SCH_Add_Task()
-
-while True:
-    scheduler.SCH_Update()
-    scheduler.SCH_Dispatch_Tasks()
-    time.sleep(1)
+class Task1:
+    def __init__(self):
+        print("Init task 1")
+        return
+    
+    def Task1_Run(self):
+        print("Task 1 is activated!!!")
